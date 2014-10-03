02cbc0387d96cbe9eadbfac5f570572ce946f0b9
Karthik <kkarty@live.com>
2014-10-03 16:17:05 +0000

    fixed nav UI for landing page

--- app/config/passport.js
+++ app/config/passport.js
@@ -2,7 +2,7 @@
 
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-var User = require('../app/models/user');
+var User = require('../models/user');
 
 var configAuth = require('./auth');
 
